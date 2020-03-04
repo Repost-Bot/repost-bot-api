d9aa28d1493a0dd895e21f099cdc96cd0c640f83
Pavel Luhin <pavel.luhin@moneyman.ru>
2020-03-04 23:58:53 +0300

    change queue item date added

--- src/main/java/com/pluhin/repostbot/RepostBotApp.java
+++ src/main/java/com/pluhin/repostbot/RepostBotApp.java
@@ -3,9 +3,11 @@ package com.pluhin.repostbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableScheduling
 @SpringBootApplication
 public class RepostBotApp {
 
