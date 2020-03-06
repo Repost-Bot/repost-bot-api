d09757a8aa976394a5d6f328d97550eea59bfbe4
Pavel Luhin <pavel.luhin@moneyman.ru>
2020-03-06 22:59:26 +0300

    add logs

--- src/main/java/com/pluhin/repostbot/service/createqueue/DefaultCreateQueueService.java
+++ src/main/java/com/pluhin/repostbot/service/createqueue/DefaultCreateQueueService.java
@@ -5,8 +5,12 @@ import com.pluhin.repostbot.model.domainid.SourceDomainId;
 import com.pluhin.repostbot.service.SystemSettingsService;
 import com.pluhin.repostbot.service.getposts.GetPostsService;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultCreateQueueService implements CreateQueueService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DefaultCreateQueueService.class);
 
   private final GetPostsService getPostsService;
   private final SystemSettingsService systemSettingsService;
@@ -21,6 +25,7 @@ public class DefaultCreateQueueService implements CreateQueueService {
   public List<PostDTO> createQueue(SourceDomainId domainId, String queueId, List<Integer> hours) {
     Long count = ((Integer) hours.size()).longValue();
     Long offset = Long.valueOf(systemSettingsService.getProperty("queue.default.offset"));
+    LOGGER.info("Fetching queue poosts with count {} and offset {}", count, offset);
     return getPostsService.getPosts(domainId, count, offset);
   }
 }
